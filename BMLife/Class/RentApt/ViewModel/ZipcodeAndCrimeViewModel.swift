78fbdbd1351ecbe97e3f3dce77726b90a867bce7
Zijia Zhai <zijiazhai@gmail.com>
2019-06-14 16:17:19 -0500

    06/14

--- BMLife/Class/RentApt/ViewModel/ZipcodeAndCrimeViewModel.swift
+++ BMLife/Class/RentApt/ViewModel/ZipcodeAndCrimeViewModel.swift
@@ -27,18 +27,20 @@ extension ZipcodeAndCrimeViewModel {
     func loadZipcodeAndCrime(dictValue: String, finished: @escaping () -> Void) {
         datas.removeAll()
         ZJPrint(dictValue)
-        let urlStr = "\(baseAPI)api/v1/location/zipcode/areas/?page_size=300"
+        let urlStr = "\(baseAPI)api/v1/location/zipcode/areas/?page_size=50"
         NetworkTools.requestData(.post, URLString: urlStr, parameters: ["in_polygon": dictValue]) { (result) in
             guard let dict = result as? [String: Any] else { return }
             guard let jsonData = try? JSONSerialization.data(withJSONObject: dict, options: []) else {return}
             do {
                 let data = try JSONDecoder().decode(ZipcodeAndCrimeModel.self, from: jsonData)
                 guard let features = data.features else {return}
+                ZJPrint(features)
                 for feature in features {
                     guard let geometry = feature.geometry else {return}
                     guard let coordinates = geometry.coordinates else {return}
                     guard let coordinate = coordinates.first?.first else {return}
                     guard let property = feature.properties else {return}
+                    ZJPrint(property.crimecount)
                     guard let crimeCount = property.crimecount else {return}
                     guard let zipcode = property.zcta5ce10 else {return}
                     let tempData = ZipcodeCrimeAndCount(count: crimeCount, coordinates: coordinate, zipcode: zipcode)
